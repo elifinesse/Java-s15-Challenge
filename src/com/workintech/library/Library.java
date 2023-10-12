682c2ca250fe499759fa6588c4713581784dce70
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-10-12 21:17:07 +0300

    library class and some attempts

--- src/com/workintech/library/Library.java
+++ src/com/workintech/library/Library.java
@@ -3,6 +3,7 @@ package com.workintech.library;
 import java.util.List;
 
 import com.workintech.library.people.Author;
+import com.workintech.library.people.Librarian;
 import com.workintech.library.people.Member;
 import com.workintech.library.literature.Book;
 import com.workintech.library.literature.Journal;
@@ -16,4 +17,70 @@ public class Library {
     private List<Journal> journals;
     private List<Member> members;
     private List<Author> authors;
+    private List<Librarian> librarians;
+
+    public Library(List<Literature> allLiterature, List<Book> books, List<Magazine> magazines, List<Journal> journals, List<Member> members, List<Author> authors, List<Librarian> librarians){
+        this.allLiterature = allLiterature;
+        this.books = books;
+        this.magazines = magazines;
+        this.journals = journals;
+        this.members = members;
+        this.authors = authors;
+        this.librarians = librarians;
+    }
+    public List<Literature> getAllLiterature() {
+        return allLiterature;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Journal> getJournals() {
+        return journals;
+    }
+
+    public List<Librarian> getLibrarians() {
+        return librarians;
+    }
+
+    public List<Magazine> getMagazines() {
+        return magazines;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setAllLiterature(List<Literature> allLiterature) {
+        this.allLiterature = allLiterature;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    public void setJournals(List<Journal> journals) {
+        this.journals = journals;
+    }
+
+    public void setLibrarians(List<Librarian> librarians) {
+        this.librarians = librarians;
+    }
+
+    public void setMagazines(List<Magazine> magazines) {
+        this.magazines = magazines;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 }
--- src/com/workintech/library/Main.java
+++ src/com/workintech/library/Main.java
@@ -1,10 +1,29 @@
 package com.workintech.library;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
+import com.workintech.library.literature.Book;
+import com.workintech.library.literature.Category;
+import com.workintech.library.people.Author;
+import com.workintech.library.people.Member;
+import com.workintech.library.people.MembershipType;
+import com.workintech.library.people.Person;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Person member1 = new Member("1", "Roy, Logan", MembershipType.SENIOR);
+        Author author = new Author("1", "shakespeare", null);
+        Author author2 = new Author("2", "scandinavian myth", null);
+        Set<Author> authors = new TreeSet<>();
+        authors.add(author); 
+        authors.add(author2);
+        Book book = new Book(1, "hamlet", 2, false, authors, null, Category.DRAMA);
+        List<Book> borrowedbooks = new ArrayList<>();
+        borrowedbooks.add(book); 
+        ((Member)member1).borrowBook(book);
+        System.out.println(member1);
     }
 }
